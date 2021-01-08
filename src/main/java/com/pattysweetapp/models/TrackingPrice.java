8bd4de31af606fb0e7597ab6b12a9966a0f9631f
patismyname <patismyname@gmail.com>
2021-01-08 22:23:06 +0700

    Pattana.k updated

--- src/main/java/com/pattysweetapp/models/TrackingPrice.java
+++ src/main/java/com/pattysweetapp/models/TrackingPrice.java
@@ -23,6 +23,14 @@ public class TrackingPrice {
 
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     public String getId() {
         return id;
     }
