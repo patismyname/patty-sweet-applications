aa1b5ed6f32acc1424debf1b0e81254354be3710
patismyname <patismyname@gmail.com>
2021-01-08 22:21:24 +0700

    Pattana.k updated

--- src/main/java/com/pattysweetapp/models/TrackingPrice.java
+++ src/main/java/com/pattysweetapp/models/TrackingPrice.java
@@ -14,6 +14,7 @@ public class TrackingPrice {
     private String  nickName;
     private Date createdDate;
     private Date updatedDate;
+    private  String status;
 
     private Product product;
     private Customer customer;
